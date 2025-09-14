16bff6fac0f7622ca4d51c8e83cbb65fa1a289b2
calvogenerico <miguelfeliped@gmail.com>
2025-09-14 15:05:36 -0300

    improvements in tests.

--- pkgs/am-i-testing/test/compile.test.ts
+++ pkgs/am-i-testing/test/compile.test.ts
@@ -1,34 +1,49 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it as baseIt } from "vitest";
 import { readFile } from 'node:fs/promises'
 import { CircomCompiler } from "../src/circom-compiler.ts";
 
+type Fixture = {
+  compiler: CircomCompiler
+}
+
+const it = baseIt.extend<Fixture>({
+  compiler: async ({}, use) => {
+    const compiler = new CircomCompiler();
+    use(new CircomCompiler());
+    await compiler.clean();
+  }
+});
+
+// multi line string
+function mls(...lines: string[]) {
+  return lines.join('\n');
+}
 
 describe('compile cmd', () => {
-  it('writes source code in disk', async () => {
-    const compiler = new CircomCompiler();
-    const source = `
-pragma circom 2.2.2;
-template Test() {}
-component main = Test();
-    `;
+  it('writes source code in disk', async ({compiler}) => {
+    const source = mls(
+      'pragma circom 2.2.2;',
+      'template Test() {}',
+      'component main = Test();'
+    );
+
     const circuit = await compiler.compileStr(source);
 
     const read = await readFile(circuit.mainFilePath);
     expect(read.toString().trim()).toEqual(source.trim());
-    await compiler.clean();
   });
 
-  it('generates sym file', async () => {
-    const compiler = new CircomCompiler();
-    const source = `
-pragma circom 2.2.2;
-template Test() {
-  input signal a;
-  output signal b;
-  b <== a + 1;
-}
-component main = Test();
-    `;
+  it('generates sym file', async ({compiler}) => {
+    const source = mls(
+      'pragma circom 2.2.2;',
+      'template Test() {',
+      '  input signal a;',
+      '  output signal b;',
+      '  b <== a + 1;',
+      '}',
+      'component main = Test();'
+    );
+
     const circuit = await compiler.compileStr(source);
     const symContent = await circuit.symFile();
 
@@ -38,7 +53,5 @@ component main = Test();
     ].join('\n');
 
     expect(symContent.trim()).toEqual(expectedContent.trim())
-    await compiler.clean();
   });
-
-})
+});
