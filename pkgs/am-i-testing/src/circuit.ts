db2c3ac2e9088e0eaf3ea50a563c8688ba3761d6
calvogenerico <miguelfeliped@gmail.com>
2025-09-14 15:34:26 -0300

    witness calculation working

--- pkgs/am-i-testing/src/circuit.ts
+++ pkgs/am-i-testing/src/circuit.ts
@@ -1,5 +1,6 @@
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 import { join, parse } from "node:path";
+import { wtns, type CircuitSignals } from 'snarkjs';
 
 export class Circuit {
   mainFilePath: string;
@@ -16,5 +17,33 @@ export class Circuit {
 
   async symFile(): Promise<string> {
     return readFile(join(this.artifactDir, `${this.name}.sym`)).then(buf => buf.toString());
+  }
+
+  r1csPath(): string {
+    return join(this.artifactDir, `${this.name}.r1cs`);
+  }
+
+  async setInput(inputsObj: CircuitSignals): Promise<void> {
+    await writeFile(this.inputPath(), JSON.stringify(inputsObj, null, 2));
+  }
+
+  inputPath(): string {
+    return join(this.artifactDir, `witness-inputs.json`);
+  }
+
+  async witness(): Promise<string> {
+    const text = await readFile(this.inputPath()).then(buf => buf.toString());
+    const outPath = this.witnessPath();
+    await wtns.calculate(
+      JSON.parse(text) as CircuitSignals,
+      join(this.artifactDir, `${this.name}_js`, `${this.name}.wasm`),
+      outPath
+    );
+
+    return outPath;
+  }
+
+  witnessPath(): string {
+    return join(this.artifactDir, `trace.wts`);
   }
 }
--- pkgs/am-i-testing/test/compile.test.ts
+++ pkgs/am-i-testing/test/compile.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it as baseIt } from "vitest";
 import { readFile } from 'node:fs/promises'
 import { CircomCompiler } from "../src/circom-compiler.ts";
+import { r1cs } from 'snarkjs';
 
 type Fixture = {
   compiler: CircomCompiler
@@ -53,5 +54,60 @@ describe('compile cmd', () => {
     ].join('\n');
 
     expect(symContent.trim()).toEqual(expectedContent.trim())
+  });
+
+  it('produces right r1cs file', async ({compiler}) => {
+    const source = mls(
+      'pragma circom 2.2.2;',
+      'template Test() {',
+      '  input signal a;',
+      '  output signal b;',
+      '  b <== a + 1;',
+      '}',
+      'component main = Test();'
+    );
+
+    const circuit = await compiler.compileStr(source);
+    const path = circuit.r1csPath();
+
+    await expect(r1cs.info(path)).resolves.not.toThrow();
+  });
+
+  it('can store input in disk', async ({compiler}) => {
+    const source = mls(
+      'pragma circom 2.2.2;',
+      'template Test() {',
+      '  input signal a;',
+      '  output signal b;',
+      '  b <== a + 1;',
+      '}',
+      'component main = Test();'
+    );
+
+    const circuit = await compiler.compileStr(source);
+    await circuit.setInput({a: '11'});
+    const path = circuit.inputPath();
+
+    const file = await readFile(path);
+    expect(JSON.parse(file.toString())).toEqual({a: '11'})
+  });
+
+  it('generates witness with proper input', async ({compiler}) => {
+    const source = mls(
+      'pragma circom 2.2.2;',
+      'template Test() {',
+      '  input signal a;',
+      '  output signal b;',
+      '  b <== a + 1;',
+      '}',
+      'component main = Test();'
+    );
+
+    const circuit = await compiler.compileStr(source);
+    await circuit.setInput({a: '11'});
+    const path = await circuit.witness();
+
+    const file = await readFile(path);
+    expect(file.toString()).toMatch(/^wtns/);
   });
 });
